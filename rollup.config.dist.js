77629b888ee03dc160c62c37771df66adb2017f9
nikotomad <nikotomad@gmail.com>
2021-02-17 00:12:37 +0100

    added scaffolding, rollup, tsconfig, phaser

---
+++ rollup.config.dist.js
@@ -0,0 +1,49 @@
+import commonjs from 'rollup-plugin-commonjs';
+import resolve from 'rollup-plugin-node-resolve';
+import replace from '@rollup/plugin-replace';
+import { uglify } from 'rollup-plugin-uglify';
+import typescript from 'rollup-plugin-typescript2';
+
+export default {
+    input: [
+        './src/game.ts'
+    ],
+    output: {
+        file: './dist/game.js',
+        name: 'MyGame',
+        format: 'iife',
+        sourcemap: false,
+    },
+
+    plugins: [
+
+        //  Toggle the booleans here to enable / disable Phaser 3 features:
+        replace({
+            'typeof CANVAS_RENDERER': JSON.stringify(true),
+            'typeof WEBGL_RENDERER': JSON.stringify(true),
+            'typeof EXPERIMENTAL': JSON.stringify(true),
+            'typeof PLUGIN_CAMERA3D': JSON.stringify(false),
+            'typeof PLUGIN_FBINSTANT': JSON.stringify(false),
+            'typeof FEATURE_SOUND': JSON.stringify(true)
+        }),
+
+        //  Parse our .ts source files
+        resolve({
+            extensions: [ '.ts', '.tsx' ]
+        }),
+        //  We need to convert the Phaser 3 CJS modules into a format Rollup can use:
+        commonjs({
+            include: [
+                'node_modules/eventemitter3/**',
+                'node_modules/phaser/**'
+            ],
+            exclude: [ 
+                'node_modules/phaser/src/polyfills/requestAnimationFrame.js'
+            ],
+            sourceMap: false,
+            ignoreGlobal: true
+        }),
+        typescript(),
+        uglify({ mangle: false })
+    ]
+};
